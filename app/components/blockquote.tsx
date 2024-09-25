700dff093183ecbea178f0670cacf54fa79b50d6
Kota Cody Enokida <codyenokida@gmail.com>
2024-09-26 00:47:55 +0900

    edit bloquote style and new post

--- app/components/blockquote.tsx
+++ app/components/blockquote.tsx
@@ -9,16 +9,14 @@ export default function BlockQuote({
 }: BlockQuoteProps) {
   return (
     <div
-      className={`relative h-fit grid grid-cols-[8px_1fr] rounded-md gap-3 bg-green-faded pr-3 ${
+      className={`relative h-fit rounded-md bg-green-faded p-3 mt-[4px] ${
         type === "neutral" ? "text-neutral-800" : "text-red-500"
       }`}
     >
-      <div
-        className={`w-2 h-full rounded-l-md ${
-          type === "neutral" ? "bg-green" : "bg-red-500"
-        }`}
-      />
-      <div>{children}</div>
+      <div className="absolute top-[-8px] left-[-8px] w-8 h-8 bg-green rounded-full flex items-center justify-center border-[3px] border-white">
+        <span className="text-sm text-white pointer-events-none">i</span>
+      </div>
+      <div className="m-0 p-0 pl-5">{children}</div>
     </div>
   );
 }
