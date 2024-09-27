1bb5806acb58f59a27448c2f970ec229f10cd4de
Kota Cody Enokida <codyenokida@gmail.com>
2024-09-27 18:06:45 +0900

    fix some styling

--- app/components/blockquote.tsx
+++ app/components/blockquote.tsx
@@ -9,14 +9,14 @@ export default function BlockQuote({
 }: BlockQuoteProps) {
   return (
     <div
-      className={`relative h-fit rounded-md bg-green-faded p-3 mt-[4px] ${
+      className={`blockquote relative h-fit rounded-md bg-green-faded p-2 mt-[4px] ${
         type === "neutral" ? "text-neutral-800" : "text-red-500"
       }`}
     >
-      <div className="absolute top-[-8px] left-[-8px] w-8 h-8 bg-green rounded-full flex items-center justify-center border-[3px] border-white">
+      <div className="absolute top-[-12px] left-[-12px] w-8 h-8 bg-green rounded-full flex items-center justify-center border-[3px] border-white">
         <span className="text-sm text-white pointer-events-none">i</span>
       </div>
-      <div className="m-0 p-0 pl-5">{children}</div>
+      <div className="m-0 p-0 pl-4">{children}</div>
     </div>
   );
 }
