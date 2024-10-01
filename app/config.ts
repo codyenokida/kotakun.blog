53578381e882a24e623a1f9d676bdb9e7245e07a
Kota Cody Enokida <codyenokida@gmail.com>
2024-10-02 00:31:17 +0900

    [NEW POST] Reasons to blog; update config

--- app/config.ts
+++ app/config.ts
@@ -3,9 +3,9 @@ import { getLatestPostMetadata, getPostMetadataBySlug } from "./blog/utils";
 // top four posts to recommend to users
 export const RECOMMENDED_POSTS = [
   getLatestPostMetadata(),
+  getPostMetadataBySlug("taiwan-update"),
   getPostMetadataBySlug("too-many-blogs"),
   getPostMetadataBySlug("vietnam-three"),
-  getPostMetadataBySlug("living-close-to-friends"),
 ];
 
 export const PROJECTS: Project[] = [
